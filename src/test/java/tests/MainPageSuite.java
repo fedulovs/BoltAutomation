b3dd419f05c078bf19f74082a5baf5822b597a1f
s.fedulov <s.fedulov@corp.vk.com>
2021-12-18 18:41:14 +0300

    Add Allure id annotation to bind cases to Testops

--- src/test/java/tests/MainPageSuite.java
+++ src/test/java/tests/MainPageSuite.java
@@ -1,5 +1,6 @@
 package tests;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Description;
 import models.SocialNetwork;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +12,7 @@ public class MainPageSuite extends TestBase {
     BoltMainPage mainPage = new BoltMainPage();
 
     @Test
+    @AllureId("6226")
     @DisplayName("Open main page")
     @Description("Open main page and check if navbar items exist")
     public void openMainPage() {
@@ -18,6 +20,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6227")
     @DisplayName("Open drivers page")
     @Description("Open drivers page and check if it contains text for drivers")
     public void openDriversPage() {
@@ -28,6 +31,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6228")
     @DisplayName("Open fleets page")
     @Description("Open fleets page and check if it contains text for fleets")
     public void openFleetsPage() {
@@ -38,6 +42,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6229")
     @DisplayName("Open App Store")
     @Description("Open App Store and check if app is there")
     public void openAppStore() {
@@ -48,6 +53,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6230")
     @DisplayName("Open Google Play")
     @Description("Open Google Play and check if app is there")
     public void openGooglePlay() {
@@ -58,6 +64,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6231")
     @DisplayName("Open Twitter")
     @Description("Check if twitter icon leads to twitter page")
     public void openTwitter() {
@@ -69,6 +76,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6232")
     @DisplayName("Open Instagram")
     @Description("Check if Instagram icon leads to Instagram page")
     public void openInstagram() {
@@ -80,6 +88,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6233")
     @DisplayName("Open Linkedin")
     @Description("Check if Linkedin icon leads to Linkedin page")
     public void openLinkedin() {
@@ -91,6 +100,7 @@ public class MainPageSuite extends TestBase {
     }
 
     @Test
+    @AllureId("6234")
     @DisplayName("Open TikTok")
     @Description("Check if TikTok icon leads to TikTok page")
     public void openTikTok() {
